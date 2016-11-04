6060f57e75aa83e46794f2fbbe4335d8e47f0691
Carlos Gochez <cgotcha@gmail.com>
2016-11-03 20:03:35 -0600

    Agregar un nuevo trofeo

--- app/controllers/trophies_controller.rb
+++ app/controllers/trophies_controller.rb
@@ -1,7 +1,29 @@
 class TrophiesController < ApplicationController
   def new
+  	@student = Student.find(params[:student_id])
+  	@trophy = @student.trophies.build
+  end
+
+  def create
+  	@student = Student.find(params[:student_id])
+  	@trophy = @student.trophies.build(trophy_params)
+  
+  	if @trophy.save
+  		redirect_to @student
+  	else
+  		render :new
+  	end
   end
 
   def edit
   end
+
+  def update
+  end
+
+  protected
+
+  	def trophy_params
+  		params.require(:trophy).permit(:description, :points)
+  	end
 end
