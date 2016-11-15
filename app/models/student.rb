1786e1ff454bf48958fe306d8b2fae3d352ccf4d
Carlos Gochez <cgotcha@gmail.com>
2016-11-14 20:05:20 -0600

    Ok ok ahora si los callbacks

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,24 @@ class Student < ActiveRecord::Base
 	#validates :name, presence: true
 
 	has_many :trophies
+	belongs_to :career
+
+	before_create :check_birthday, :first_trophy
+	#before_validation :hack
+
+	protected
+
+		def check_birthday
+			puts "Llamo a check_birthday"
+			if birthday_at.blank?
+				self.birthday_at = Date.current
+			end
+		end
+
+		def first_trophy
+		end
+
+		def hack
+			errors[:base] << "because i want to"
+		end
 end
